4e688ba9395900ece313dd423806f8c00816ab72
kunsay <kunsay0326@hotmail.co.jp>
2019-08-08 10:37:21 +0900

    design-hidokitsushin
    非同期通信の実装です。(2019/8/8)

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,56 @@
+$(function(){
+
+
+    function buildMessage(message){
+      var img = ""
+        if (message.image.url !== null) {
+          img = `<img src = "${message.image.url}">`
+      }
+      var html = `<div class="message">
+                  <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.created_at}
+                  </div>
+                  </div>
+                  <div class="lower-message">
+                  <p class="lower-message__content">
+                    ${message.content}
+                    ${img}
+                  </p>
+      
+                  </div>
+                  </div>`
+      return html;
+    }
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType : 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      console.log(message);
+      var html = buildMessage(message);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('.messages').append(html)
+      $('#message_content').val('')
+      $('#message_image').val('')
+      $('.form__submit').attr('disabled', false); 
+    })
+    .fail(function(){
+      alert('エラー');
+      $('.form__submit').attr('disabled', false); 
+    })
+  })
+});
